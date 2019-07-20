db5d79de9cffa4a8bc249974477b7ccbced06e1b
Jesse Zhang <sbjesse@gmail.com>
2019-07-19 17:43:53 -0700

    Emit diagnostics

--- main.cc
+++ main.cc
@@ -2,6 +2,11 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
+#include "clang/Frontend/TextDiagnostic.h"
+
+namespace Rofl {
+using clang::ast_matchers::anyOf;
+using clang::ast_matchers::anything;
 using clang::ast_matchers::cxxConstructExpr;
 using clang::ast_matchers::cxxConstructorDecl;
 using clang::ast_matchers::cxxCtorInitializer;
@@ -16,17 +21,71 @@ using clang::ast_matchers::has;
 using clang::ast_matchers::hasDeclaration;
 using clang::ast_matchers::hasParent;
 using clang::ast_matchers::hasType;
+using clang::ast_matchers::MatchFinder;
 using clang::ast_matchers::materializeTemporaryExpr;
 using clang::ast_matchers::parmVarDecl;
 using clang::ast_matchers::qualType;
 using clang::ast_matchers::references;
 using clang::ast_matchers::to;
+using clang::ast_matchers::varDecl;
 using clang::ast_matchers::withInitializer;
 
-struct Yolo : clang::ast_matchers::MatchFinder::MatchCallback {
-  void run(
-      const clang::ast_matchers::MatchFinder::MatchResult& Result) override {}
+const auto kPassesTempToMemoizedRef = forEachArgumentWithParam(
+    expr(hasParent(materializeTemporaryExpr())).bind("Problem"),
+    parmVarDecl(equalsBoundNode("ParmVar")));
+const auto kForRefField = forField(hasType(references(qualType())));
+const auto kSpecialCtor =
+    cxxConstructorDecl(forEachConstructorInitializer(cxxCtorInitializer(
+        kForRefField,
+        withInitializer(declRefExpr(to(parmVarDecl().bind("ParmVar")))))));
+const auto kMatcher =
+    exprWithCleanups(has(cxxConstructExpr(hasDeclaration(kSpecialCtor),
+                                          kPassesTempToMemoizedRef)),
+                     anyOf(expr(hasParent(varDecl().bind("Var"))), anything()))
+        .bind("root");
+
+struct Yolo : MatchFinder::MatchCallback {
+  void run(const MatchFinder::MatchResult& result) override {
+    auto temporary = result.Nodes.getNodeAs<clang::Expr>("Problem");
+    auto root = result.Nodes.getNodeAs<clang::ExprWithCleanups>("root");
+
+    auto var = result.Nodes.getNodeAs<clang::VarDecl>("Var");
+
+    std::string kString = var->getName();
+
+    const clang::SourceRange& range = temporary->getSourceRange();
+    clang::TextDiagnostic TD(
+        llvm::outs(), result.Context->getLangOpts(),
+        &result.Context->getDiagnostics().getDiagnosticOptions());
+    TD.emitDiagnostic(clang::FullSourceLoc(range.getBegin(),
+                                           result.Context->getSourceManager()),
+                      clang::DiagnosticsEngine::Note,
+                      "Materializing a temporary here, but it will be "
+                      "destroyed immediately after constructing the object \"" +
+                          kString + "\"",
+                      clang::CharSourceRange::getTokenRange(range), llvm::None);
+  }
 };
+
+class Fomo {
+  MatchFinder finder_;
+  Yolo yolo_;
+  std::unique_ptr<clang::tooling::FrontendActionFactory> frontendActionFactory_;
+
+ public:
+  Fomo()
+      : frontendActionFactory_(
+            clang::tooling::newFrontendActionFactory(&finder_)) {
+    finder_.addMatcher(kMatcher, &yolo_);
+  }
+  clang::tooling::ToolAction* getFrontEndAction() {
+    return frontendActionFactory_.get();
+  }
+};
+
+Fomo Action() { return {}; }
+}  // namespace Rofl
+
 int main(int argc, const char** argv) {
   llvm::cl::OptionCategory ctros_saving_refs_category(
       "dummy tool option category");
@@ -35,19 +94,5 @@ int main(int argc, const char** argv) {
   clang::tooling::ClangTool tool(parser.getCompilations(),
                                  parser.getSourcePathList());
 
-  const auto kPassesTempToMemoizedRef = forEachArgumentWithParam(
-      expr(hasParent(materializeTemporaryExpr())).bind("Problem"),
-      parmVarDecl(equalsBoundNode("ParmVar")));
-  const auto kForRefField = forField(hasType(references(qualType())));
-  const auto kSpecialCtor =
-      cxxConstructorDecl(forEachConstructorInitializer(cxxCtorInitializer(
-          kForRefField,
-          withInitializer(declRefExpr(to(parmVarDecl().bind("ParmVar")))))));
-  const auto kMatcher = exprWithCleanups(has(cxxConstructExpr(
-      hasDeclaration(kSpecialCtor), kPassesTempToMemoizedRef)));
-
-  clang::ast_matchers::MatchFinder finder;
-  Yolo yolo;
-  finder.addMatcher(kMatcher, &yolo);
-  tool.run(clang::tooling::newFrontendActionFactory(&finder).get());
+  tool.run(Rofl::Action().getFrontEndAction());
 }
