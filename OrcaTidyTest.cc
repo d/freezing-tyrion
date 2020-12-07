6241a6d79d9fa6950f35a26b6fb7094e55f2e52b
Jesse Zhang <sbjesse@gmail.com>
2020-12-07 15:00:22 -0800

    Extract common test set up into a fixture

--- OrcaTidyTest.cc
+++ OrcaTidyTest.cc
@@ -9,14 +9,51 @@
 using ::testing::SizeIs;
 using ::testing::StrEq;
 
-TEST(OrcaTidyTest, FieldOwnRelease) {
-  llvm::cl::OptionCategory category("my-tool options");
-  const char* const sourceFilePath = "foo.cc";
-  const char* argv[] = {"annotate", sourceFilePath, "--", "-xc++"};
-  int argc = std::size(argv);
+class OrcaTidyTest : public ::testing::Test {
+ protected:
+  clang::tooling::CommonOptionsParser parser_;
+  clang::tooling::RefactoringTool tool_;
+  constexpr static const char* const kSourceFilePath = "/tmp/foo.cc";
 
-  clang::tooling::CommonOptionsParser parser(argc, argv, category);
+  auto& GetFileToReplaces() { return tool_.getReplacements(); }
 
+  void TestBeforeAfter(const std::string& code,
+                       const std::string& expected_changed_code) {
+    tool_.mapVirtualFile(kSourceFilePath, code);
+
+    auto& file_to_replaces = GetFileToReplaces();
+    orca_tidy::AnnotateAction action(file_to_replaces);
+
+    int exit_code =
+        tool_.run(clang::tooling::newFrontendActionFactory(&action).get());
+
+    ASSERT_EQ(0, exit_code);
+
+    ASSERT_THAT(file_to_replaces, SizeIs(1));
+    const auto& replacements = file_to_replaces.begin()->second;
+
+    auto changed_code =
+        clang::tooling::applyAllReplacements(code, replacements);
+
+    if (!changed_code) FAIL() << "failed applying replacements";
+
+    // TODO: nicer matcher that formats before comparison
+    ASSERT_THAT(expected_changed_code, StrEq(*changed_code));
+  }
+
+ public:
+  OrcaTidyTest()
+      : parser_([] {
+          llvm::cl::OptionCategory category("my-tool_ options");
+          const char* argv[] = {"annotate", kSourceFilePath, "--", "-xc++"};
+          int argc = std::size(argv);
+
+          return clang::tooling::CommonOptionsParser{argc, argv, category};
+        }()),
+        tool_(parser_.getCompilations(), parser_.getSourcePathList()) {}
+};
+
+TEST_F(OrcaTidyTest, FieldOwnRelease) {
   std::string code = R"C++(
     struct T {
       void Release();
@@ -25,27 +62,8 @@ TEST(OrcaTidyTest, FieldOwnRelease) {
     struct R {
       T* t;
       ~R() { t->Release(); }
-    };)C++";
-
-  clang::tooling::RefactoringTool tool(parser.getCompilations(),
-                                       parser.getSourcePathList());
-  tool.mapVirtualFile(sourceFilePath, code);
-
-  orca_tidy::AnnotateAction action(tool.getReplacements());
-
-  int exit_code =
-      tool.run(clang::tooling::newFrontendActionFactory(&action).get());
-
-  ASSERT_EQ(0, exit_code);
-
-  ASSERT_THAT(tool.getReplacements(), SizeIs(1));
-  const auto& replacements = tool.getReplacements().begin()->second;
-
-  auto changed_code = clang::tooling::applyAllReplacements(code, replacements);
-
-  if (!changed_code) FAIL() << "failed applying replacements";
-
-  auto expected_changed_code = R"C++(
+    };)C++",
+              expected_changed_code = R"C++(
     struct T {
       void Release();
     };
@@ -55,6 +73,5 @@ TEST(OrcaTidyTest, FieldOwnRelease) {
       ~R() { t->Release(); }
     };)C++";
 
-  // TODO: nicer matcher that formats before comparison
-  ASSERT_THAT(expected_changed_code, StrEq(*changed_code));
+  TestBeforeAfter(code, expected_changed_code);
 }
