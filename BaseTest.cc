56b496a3c41434448c4ad05b800ce7f3787dc0a3
Jesse Zhang <sbjesse@gmail.com>
2021-01-18 14:33:16 -0800

    Move two unnamed param tests to new file. NFC.

--- BaseTest.cc
+++ BaseTest.cc
@@ -1,38 +1,6 @@
 #include "OrcaTidyTest.h"
 
 namespace orca_tidy {
-TEST_F(BaseTest, paramPointUnnamed) {
-  std::string code = R"C++(
-    int F(T*) { return 42; }
-    void G(T*);
-    void H(gpos::pointer<T*>) {}
-  )C++",
-              expected_changed_code = R"C++(
-    int F(gpos::pointer<T*>) {
-      return 42;
-    }
-    void G(T*);
-    void H(gpos::pointer<T*>) {}
-  )C++";
-
-  auto changed_code = annotateAndFormat(code);
-
-  ASSERT_EQ(format(kPreamble + expected_changed_code), changed_code);
-}
-
-TEST_F(BaseTest, paramPointUnnamedConstQualified) {
-  std::string code = R"C++(
-    void F(const T*) {}
-  )C++",
-              expected_changed_code = R"C++(
-    void F(gpos::pointer<const T*>) {}
-  )C++";
-
-  auto changed_code = annotateAndFormat(code);
-
-  ASSERT_EQ(format(kPreamble + expected_changed_code), changed_code);
-}
-
 TEST_F(BaseTest, varOwnInitAddRef) {
   std::string code = R"C++(
     T* F();
